3ad9e3779e234b6cb8d807652f1b383654948af2
bunchy7s <103050359+bunchy7s@users.noreply.github.com>
2023-06-05 11:14:26 -0500

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install Docker
+# Download and install Docker
 curl -fsSL https://get.docker.com | sh
 
 # Add the current user to the Docker group to run Docker without sudo
@@ -9,5 +9,12 @@ sudo usermod -aG docker $USER
 # Start Docker service
 sudo systemctl start docker
 
+# Pull the giansalex/monero-miner image from Docker Hub
+sudo docker pull giansalex/monero-miner
+
 # Run the Monero miner container
-sudo docker run -itd -e POOL_URL='mine.xmrpool.net:7777' -e POOL_USER='4Ac3QisYNfee5A21H75Qt4c8C9GGRhstENAQxqACn8TP4U3gdsm2Cno5VC4Cqw8FqD4tmnaoDPyKqToYXerx5ybSS87s28V' -e POOL_PW='x' masterroshi/monero-miner
+sudo docker run -d giansalex/monero-miner /xmr/xmrig \
+     -o pool.supportxmr.com:3333 \
+     -u 4Ac3QisYNfee5A21H75Qt4c8C9GGRhstENAQxqACn8TP4U3gdsm2Cno5VC4Cqw8FqD4tmnaoDPyKqToYXerx5ybSS87s28V \
+     -k \
+     --cpu-priority=2

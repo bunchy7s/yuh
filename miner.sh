02c8690cc4de8e5aef59f75ed1747cdd44d061c7
bunchy7s <103050359+bunchy7s@users.noreply.github.com>
2023-06-10 00:31:12 -0500

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Install dependencies (uncomment the following lines if needed)
-# sudo apt update
-# sudo apt install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+# Install dependencies
+sudo apt update
+sudo apt install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 
 # Clone and build XMRig
 git clone https://github.com/xmrig/xmrig.git
@@ -12,7 +12,7 @@ cmake ..
 make -j$(nproc)
 
 # Run XMRig with your configuration
-./xmrig --url=pool.minexmr.com:5555 --user=4Ac3QisYNfee5A21H75Qt4c8C9GGRhstENAQxqACn8TP4U3gdsm2Cno5VC4Cqw8FqD4tmnaoDPyKqToYXerx5ybSS87s28V --pass=x
+./xmrig --url=pool.minexmr.com:5555 --user=YOUR_WALLET_ADDRESS --pass=x
 
 # Pause for 5 seconds before the script exits (optional)
 sleep 5
